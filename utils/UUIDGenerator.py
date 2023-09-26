483081b733c75ea7775c973bd593f87cfc133344
Ivaldir Batalha <ivalbat1@gmail.com>
2023-09-26 17:09:54 -0100

    Created functions to generate codigo and uiid for id's in cliente and produtos

--- utils/UUIDGenerator.py
+++ utils/UUIDGenerator.py
@@ -1,4 +1,35 @@
 import uuid
+import sys
 
-def get_uiid() -> None:
-    return str(uuid.uuid4())
+sys.path.insert(1, '/Users/ivaldir/Desktop/coding/ApiFaturacao')
+
+from repository.configuration.databaseConfigurationAndQuery import DatabaseConnectorAndQuery
+
+def verify_if_uiid_exists_in_database(uiid: str, uiidUseType) -> None:
+    queryMaiorCodigoNaBaseDeDados = ""
+    if uiidUseType == 1:
+        queryMaiorCodigoNaBaseDeDados = "SELECT (ID) FROM `cliente` WHERE ID = '{}';".format(uiid)
+    else:
+        queryMaiorCodigoNaBaseDeDados = "SELECT (ID) FROM `produto` WHERE ID = '{}';".format(uiid)
+        
+    mysqlDBTest = DatabaseConnectorAndQuery(
+                '127.0.0.1',
+                'faturacao',
+                'root',
+                '',
+                queryMaiorCodigoNaBaseDeDados,
+                1
+    )
+    
+    result = mysqlDBTest.connect_to_database()
+    
+    return result
+
+def get_new_uiid(uiidUseType: int) -> str:
+    newUiid = str(uuid.uuid4())
+    if verify_if_uiid_exists_in_database(newUiid, uiidUseType) == newUiid:
+        get_new_uiid(uiidUseType)
+    else:
+        return newUiid
+    
+# print(get_new_uiid(1))
--- utils/generateCodigo.py
+++ utils/generateCodigo.py
@@ -5,8 +5,13 @@ sys.path.insert(1, '/Users/ivaldir/Desktop/coding/ApiFaturacao')
 from repository.configuration.databaseConfigurationAndQuery import DatabaseConnectorAndQuery
 
 
-def check_if_new_codigo_exists_and_generate_new() -> str:
-    queryMaiorCodigoNaBaseDeDados = "SELECT (CODIGO) FROM `produto` ORDER BY CODIGO DESC LIMIT 5000;"
+def check_if_new_codigo_exists_and_generate_new(codigoUseType: int) -> str:
+    queryMaiorCodigoNaBaseDeDados = ""
+
+    if codigoUseType == 1:
+        queryMaiorCodigoNaBaseDeDados = "SELECT (CODIGO) FROM `cliente` ORDER BY CODIGO DESC LIMIT 5000;"
+    else:
+        queryMaiorCodigoNaBaseDeDados = "SELECT (CODIGO) FROM `produto` ORDER BY CODIGO DESC LIMIT 5000;"
         
     mysqlDBTest = DatabaseConnectorAndQuery(
                 '127.0.0.1',
@@ -19,9 +24,9 @@ def check_if_new_codigo_exists_and_generate_new() -> str:
     
     result = mysqlDBTest.connect_to_database()
     
-    # print(result[0])
+    print(result[0])
        
     sanitazedResult = (((((str(result[0]).replace('(', '', 1))).replace(')', '', 1)).replace(',', '', 1)).replace("'", '', 2))
     return str(int(sanitazedResult) + 1).zfill(5) 
 
-# print(check_if_new_codigo_exists_and_generate_new())
+# print(check_if_new_codigo_exists_and_generate_new(2))
